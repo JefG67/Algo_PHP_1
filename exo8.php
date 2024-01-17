9e9ded3dd0b63ddb06dbe578b0b0505f7bef230b
JefG67 <jefguilpain@gmail.com>
2024-01-17 16:55:53 +0100

    Exercice

---
+++ exo8.php
@@ -0,0 +1,11 @@
+<h1>Exercice 8</h1>
+
+<p>Ecrire un algorithme qui renvoie la table de multiplication d’un nombre passé en paramètre sous la forme<p>
+
+<h2>Résultat</h2>
+
+<?php
+
+for ($i = 1; $i <= 10; $i++) {
+
+}
