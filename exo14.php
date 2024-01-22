c62845921aaade3aeee365b2eb51b8dc3a2e80c5
JefG67 <jefguilpain@gmail.com>
2024-01-22 16:56:52 +0100

    Exo PHP

---
+++ exo14.php
@@ -0,0 +1,11 @@
+<h1>Exercice 14</h1>
+
+<p>Calculer l'âge exact d'une personne à partir de sa date de naissance (en années, mois, jours).</p>
+
+<h2> Resultat </h2>
+
+<?php
+
+$dateNaissance = "17/01/1985";
+$dateaujourdhui = date("Y-m-d");
+$diff = date_diff(date_)
